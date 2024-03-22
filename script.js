3be63f670b00ee48b3674bcfaa68682c0b07b58f
Harsh-git1 <130690167+Harsh-git1@users.noreply.github.com>
2024-03-22 19:30:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
 //complete this code
-class Animal {}
+class Animal {
+	
+	constructor(species) {
+		this.species = species;
+	}
+	get species() {
+		return this.species;
+	}
+	function makeSound () {
+		console.log("The ${this.species} makes a sound");
+	}
+}
 
-class Dog extends Animal {}
+class Dog extends Animal {
+	function bark() {
+		console.log("woof");
+	}
+}
 
-class Cat extends Animal {}
+class Cat extends Animal {
+	function purr() {
+		console.log("purr");
+	}
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
